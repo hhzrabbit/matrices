3b423efa2d3d4c751f928f7bbd1b71aad6031268
Haley Zeng <hzeng@stuy.edu>
2017-02-27 11:02:48 -0500

    :(

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import matrix
 
-m = matrix.new_matrix(3, 4)
+m = matrix.new_matrix(6, 6)
+m = matrix.ident(m)
 m = matrix.scalar_mult(m, .35)
 matrix.print_matrix(m)
-
---
+++ other.py
@@ -0,0 +1,16 @@
+from display import *
+from draw import *
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+#matrix = new_matrix()
+matrix = []
+add_point(matrix, 1, 2, 3)
+add_point(matrix, 0, 5, 10)
+add_point(matrix, 5, 2, 18)
+add_point(matrix, 4, 1, 1)
+
+
+
+#draw_lines( matrix, screen, color )
+#display(screen)
