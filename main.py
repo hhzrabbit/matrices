631e795a4520140ecf128023222c8bd099c437f0
Haley Zeng <hzeng@stuy.edu>
2017-03-01 01:48:11 -0500

    yas

--- main.py
+++ main.py
@@ -1,46 +1,53 @@
 from display import *
 from draw import *
+import random
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ random.randint(0, 255), random.randint(0, 255), random.randint(0, 255) ]
 matrix = []
 
-
-print "adding points (0, 0, 0) (100, 100, 0) (200, 0, 0) (100, 200, 0)"
+print "adding points (0, 0, 0) (50, 50, 0) (100, 0, 0) (50, 100, 0)"
 add_point(matrix, 0, 0, 0)
-add_point(matrix, 100, 100, 0)
-add_point(matrix, 200, 0, 0)
-add_point(matrix, 100, 200, 0)
+add_point(matrix, 50, 50, 0)
+add_point(matrix, 100, 0, 0)
+add_point(matrix, 50, 100, 0)
 
 print_matrix(matrix)
+print ""
 
+print "adding edge (50, 100, 0) (200, 100, 0)"
+add_edge(matrix, 50, 100, 0, 200, 100, 0)
 
-
-print "adding edge (100, 200, 0) (400, 200, 0)"
-add_edge(matrix, 100, 200, 0, 400, 200, 0)
-
-
-
-
+print_matrix(matrix)
+print ""
 
 print "identity matrix"
 identity = new_matrix()
 ident(identity)
-print_matrix(identity)
 
+print_matrix(identity)
 print ""
 
 print "scale identity matrix"
 scalar_mult(identity, 2)
-print_matrix(identity)
 
+print_matrix(identity)
 print ""
 
-print "matrix matrix mult"
+print "matrix mult: scaled identity * matrix"
 matrix_mult(identity, matrix)
-#print_matrix(ret)
 print_matrix(matrix)
+print ""
 
+#===================================================
 
-draw_lines( matrix, screen, color )
-#display(screen)
+matrix = []
+n = random.randint(5, 20)
+for i in range(n):
+    add_point(matrix, random.randint(0, 50), random.randint(0, 50))
+for trials in range(5):
+    color = [ random.randint(0, 255), random.randint(0, 255), random.randint(0, 255) ]
+    draw_lines( matrix, screen, color )
+    scalar_mult(matrix, 2)
+
+display(screen)
